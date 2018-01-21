0013ee73ac3543d46d72a78413ddac11c84056a2
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2018-01-21 17:57:53 +0800

    Create max_subarray.java

---
+++ max_subarray.java
@@ -0,0 +1,10 @@
+package max_subarray;
+
+public class max_subarray {
+public static void main(String args[])
+{
+	solution s=new solution();
+	int[] array={-2,1,-3,4,-1,2,1,-5,4};
+	s.maxSubArray(array);
+}
+}
